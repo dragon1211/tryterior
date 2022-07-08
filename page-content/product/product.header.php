2ed67bf402c66bb4cccb33ce2ff9c4b064ade116
dragon1211 <vikt0r1211@hotmail.com>
2022-07-09 02:44:55 +0900

    product.header

--- page-content/product/product.header.php
+++ page-content/product/product.header.php
@@ -21,65 +21,50 @@
         objectFitImages('.ofi');
       });
     </script>
-
-<style type="text/css">
-.header {
-  padding: 0 3rem;
-  min-height: 0rem;
-  position: relative;
-  width: 100%;
-  top: 0;
-  left: 0;
-  z-index: 99;
-  background: #fff;
-}
-.header-logo {
-  width: 32.4rem;
-}
-
-.memo {
-  font-size: 2rem;
-   text-align: center;
-   margin-top: 6rem;
-}
-
-.d-flex{
-  display: flex;
-}
-
-.justify-content-between {
-  justify-content: space-between;
-}
-
-@media screen and (max-width: 750px) {
-  .header {
-    padding: 0 1.7rem;
-    min-height: 11.4rem;
-  }
-  .header-logo {
-    width: 25.4rem;
-  }
-
-.memo {
-  font-size: 2.7rem;
-   text-align: left;
-   margin-top: 6rem;
-   margin-left: 2.5rem;
-   margin-right: 2.5rem;
-}
-
-}
-
-
-
-</style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <style type="text/css">
+      .header {
+        padding: 0 3rem;
+        min-height: 0rem;
+        position: relative;
+        width: 100%;
+        top: 0;
+        left: 0;
+        z-index: 99;
+        background: #fff;
+      }
+      .header-logo {
+        width: 32.4rem;
+      }
+      .memo {
+        font-size: 2rem;
+        text-align: center;
+        margin-top: 6rem;
+      }
+      @media screen and (max-width: 750px) {
+        .header {
+          padding: 0 1.7rem;
+          min-height: 11.4rem;
+        }
+        .header-logo {
+          width: 25.4rem;
+        }
+        .memo {
+          font-size: 2.7rem;
+          text-align: left;
+          margin-top: 6rem;
+          margin-left: 2.5rem;
+          margin-right: 2.5rem;
+        }
+      }
+    </style>
 
     <link rel="stylesheet" type="text/css" href="/css/common.css">
     <?php if (is_mobile()): ?><link rel="stylesheet" href="/css/sp_common.css" /><?php endif; ?>
     <link rel="stylesheet" type="text/css" href="/css/caselink.css">
     <script type="text/javascript" src="/js/common_new.js"></script>
 
-    
     <!-- NEW PAGE - PRODUCT -->
     <?php if(
       is_post_type_archive('product') || 
@@ -113,25 +98,25 @@
     <?php endif; ?>
 
   <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5C3LQQB');</script>
-<!-- End Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-5C3LQQB');</script>
+  <!-- End Google Tag Manager -->
 
   </head>
   <body>
-  <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5C3LQQB"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5C3LQQB"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
-<div class="page-top js-pageTop"><span></span></div>
-<header>
-  <div class="header flex">
-    <div class="header__l">
-      <h1><img src="https://tryterior.com/lpa/img/logo.png" class="header-logo" alt="トライテリア TRY-TERIOR"></h1>
-    </div>
-  </div>
-</header>
+    <div class="page-top js-pageTop"><span></span></div>
+    <header>
+      <div class="header flex">
+        <div class="header__l">
+          <h1><img src="https://tryterior.com/lpa/img/logo.png" class="header-logo" alt="トライテリア TRY-TERIOR"></h1>
+        </div>
+      </div>
+    </header>
